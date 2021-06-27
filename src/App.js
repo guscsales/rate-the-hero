de7d5340eccac8015d2a20d50fe10ee93673b167
Gustavo Sales <gustavosalessantos@live.com>
2021-06-27 20:04:01 -0300

    Upload inicial dos arquivos principais do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Search } from './screens/Search';
+import { Details } from './screens/Details';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/detalhes/:id" exact>
+					<Details />
+				</Route>
+				<Route path="/" exact>
+					<Search />
+				</Route>
+				<Route path="*">Página não encontrada</Route>
+			</Switch>
+		</BrowserRouter>
+	);
 }
-
-export default App;
