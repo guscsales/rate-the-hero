32e575cccecac7133747ffc520424325680abaf9
Gustavo Sales <gustavosalessantos@gmail.com>
2021-08-11 13:41:30 -0300

    feat: create HeroCard component and Search screen structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Search } from './screens/Search';
 import { Details } from './screens/Details';
+import { NormalizeStyles } from './shared/NormalizeStyles';
+import { Header } from './common-components/Header/Header';
 
 export function App() {
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route path="/detalhes/:id" exact>
-					<Details />
-				</Route>
-				<Route path="/" exact>
-					<Search />
-				</Route>
-				<Route path="*">Página não encontrada</Route>
-			</Switch>
-		</BrowserRouter>
+		<>
+			<NormalizeStyles />
+			<BrowserRouter>
+				<Header />
+				<Switch>
+					<Route path="/detalhes/:id" exact>
+						<Details />
+					</Route>
+					<Route path="/" exact>
+						<Search />
+					</Route>
+					<Route path="*">Página não encontrada</Route>
+				</Switch>
+			</BrowserRouter>
+		</>
 	);
 }
